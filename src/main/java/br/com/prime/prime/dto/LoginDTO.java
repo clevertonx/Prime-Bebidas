ccb51f93090c37f15bdab5a4de0f6548459b7464
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-05-04 16:35:20 -0400

    atualizando

---
+++ src/main/java/br/com/prime/prime/dto/LoginDTO.java
@@ -0,0 +1,12 @@
+package br.com.prime.prime.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    private Long id;
+} 
---
+++ src/main/java/br/com/prime/prime/dto/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package br.com.prime.prime.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+    private String email;
+    private String senha; 
+}
