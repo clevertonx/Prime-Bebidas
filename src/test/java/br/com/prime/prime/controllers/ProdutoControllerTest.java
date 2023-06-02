0a1bcd6bd80e1c862f9d4cab6f3ccc8050f5ab18
clevertonx <clevertonx@gmail.com>
2023-06-02 16:46:19 -0400

    fazer mapperProduto e atualizar controller e conferir test produtoController

--- src/test/java/br/com/prime/prime/controllers/ProdutoControllerTest.java
+++ src/test/java/br/com/prime/prime/controllers/ProdutoControllerTest.java
@@ -1,7 +1,11 @@
 package br.com.prime.prime.controllers;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -30,197 +34,184 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import br.com.prime.prime.Builders.EstabelecimentoBuilder;
+import br.com.prime.prime.Builders.Imagem;
 import br.com.prime.prime.Builders.ProdutoBuilder;
+import br.com.prime.prime.Builders.UsuarioBuilder;
+import br.com.prime.prime.dto.EstabelecimentoRequestDTO;
 import br.com.prime.prime.dto.ProdutoRequestDTO;
 import br.com.prime.prime.dto.ProdutoResponseDTO;
 import br.com.prime.prime.models.Categoria;
 import br.com.prime.prime.models.Estabelecimento;
 import br.com.prime.prime.models.Produto;
+import br.com.prime.prime.models.Usuario;
 import br.com.prime.prime.repository.EstabelecimentoRepository;
 import br.com.prime.prime.repository.ProdutoRepository;
 import br.com.prime.prime.utils.JsonUtil;
+import jakarta.transaction.Transactional;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@Transactional
 public class ProdutoControllerTest {
-    @Autowired
-    private MockMvc mvc;
 
-    @Autowired
-    private ProdutoRepository produtoRepository;
+        private ObjectMapper objectMapper = new ObjectMapper();
 
-    @Autowired
-    private EstabelecimentoRepository estabelecimentoRepository;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @BeforeEach
-    @AfterEach
-    public void deletaDados() {
-        produtoRepository.deleteAll();
-    }
+        @Autowired
+        private ProdutoRepository produtoRepository;
 
-    @Test
-    public void deve_buscar_produtos_por_nome() throws Exception {
-        Estabelecimento estabelecimento = new EstabelecimentoBuilder().construir();
-        estabelecimentoRepository.save(estabelecimento);
+        @Autowired
+        private EstabelecimentoRepository estabelecimentoRepository;
 
-        String nomeProduto = "Pinga";
-        Produto produto = new ProdutoBuilder().comNome(nomeProduto).comEstabelecimento(estabelecimento).construir();
-        produtoRepository.save(produto);
+        @BeforeEach
+        @AfterEach
+        public void deletaDados() {
+                produtoRepository.deleteAll();
+        }
 
-        MvcResult resultado = mvc.perform(get("/produto/buscarPorNome?nome=" + nomeProduto)).andReturn();
+        @Test
+        public void deve_buscar_produtos_por_nome() throws Exception {
 
-        ProdutoResponseDTO[] produtosRetornadosDTO = JsonUtil.mapFromJson(
-                resultado.getResponse().getContentAsString(),
-                ProdutoResponseDTO[].class);
+                Usuario usuario = new UsuarioBuilder().construir();
 
-        assertThat(HttpStatus.OK.value()).isEqualTo(resultado.getResponse().getStatus());
-        assertThat(produtosRetornadosDTO).extracting("nome").contains(nomeProduto);
-        assertThat(produtosRetornadosDTO).hasSize(1);
-    }
+                Estabelecimento estabelecimento = new EstabelecimentoBuilder().comUsuario(usuario).construir();
+                estabelecimentoRepository.save(estabelecimento);
 
-    @Test
-    public void deve_buscar_os_produtos_cadastrados_pela_categoria() throws Exception {
-        Categoria categoria = Categoria.Fortificada;
-        ArrayList<Produto> produtos = new ArrayList<Produto>();
-        produtos.add(new ProdutoBuilder().construir());
-        produtos.add(new ProdutoBuilder().comCategoria(categoria).construir());
+                String nomeProduto = "Pinga";
+                Produto produto = new ProdutoBuilder().comEstabelecimento(estabelecimento).comNome(nomeProduto)
+                                .construir();
+                produtoRepository.save(produto);
 
-        produtoRepository.saveAll(produtos);
+                MvcResult resultado = mockMvc.perform(get("/produto/buscarPorNome?nome=" + nomeProduto)).andReturn();
 
-        this.mvc
-                .perform(get("/produto"))
-                .andExpect(status().isOk());
-        assertEquals(Categoria.Fortificada, categoria);
-    }
+                ProdutoResponseDTO[] produtosRetornadosDTO = JsonUtil.mapFromJson(
+                                resultado.getResponse().getContentAsString(),
+                                ProdutoResponseDTO[].class);
 
-    @Test
-    void deve_excluir_um_produto_pelo_id() throws Exception {
+                assertThat(HttpStatus.OK.value()).isEqualTo(resultado.getResponse().getStatus());
+                assertThat(produtosRetornadosDTO).extracting("nome").contains(nomeProduto);
+                assertThat(produtosRetornadosDTO).hasSize(1);
+        }
 
-        Estabelecimento estabelecimento = new EstabelecimentoBuilder().construir();
-        estabelecimentoRepository.save(estabelecimento);
+        @Test
+        public void deve_buscar_os_produtos_cadastrados_pela_categoria() throws Exception {
 
-        int quantidadeEsperada = 0;
+                Usuario usuario = new UsuarioBuilder().construir();
+                Categoria categoria = Categoria.Fortificada;
 
-        Produto produto = new ProdutoBuilder().comEstabelecimento(estabelecimento).construir();
-        produtoRepository.save(produto);
+                Estabelecimento estabelecimento = new EstabelecimentoBuilder().comUsuario(usuario).construir();
+                estabelecimentoRepository.save(estabelecimento);
 
-        String url = "/produto/" + produto.getId();
-        mvc.perform(delete(url)).andReturn();
+                ArrayList<Produto> produtos = new ArrayList<Produto>();
+                produtos.add(new ProdutoBuilder().comEstabelecimento(estabelecimento).construir());
+                produtos.add(new ProdutoBuilder().comEstabelecimento(estabelecimento).comCategoria(categoria)
+                                .construir());
 
-        Iterable<Produto> produtosEncontrados = produtoRepository.findAll();
-        long quantidadeEncontrada = produtosEncontrados.spliterator().getExactSizeIfKnown();
+                produtoRepository.saveAll(produtos);
 
-        assertEquals(quantidadeEsperada, quantidadeEncontrada);
-    }
+                this.mockMvc
+                                .perform(get("/produto"))
+                                .andExpect(status().isOk());
+                assertEquals(Categoria.Fortificada, categoria);
+        }
 
-    @Test
-    void deve_cadastrar_um_novo_produto() throws Exception {
+        @Test
+        void deve_excluir_um_produto_pelo_id() throws Exception {
 
-        Estabelecimento estabelecimento = new EstabelecimentoBuilder().construir();
-        estabelecimentoRepository.save(estabelecimento);
+                Usuario usuario = new UsuarioBuilder().construir();
 
-        int quantidadeEsperada = 1;
+                Estabelecimento estabelecimento = new EstabelecimentoBuilder().comUsuario(usuario).construir();
+                estabelecimentoRepository.save(estabelecimento);
 
-        Produto produto = new ProdutoBuilder().comEstabelecimento(estabelecimento).construir();
-        produtoRepository.save(produto);
+                int quantidadeEsperada = 0;
 
-        mvc.perform(post("/produto/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(produto)))
-                .andExpect(status().isCreated());
+                Produto produto = new ProdutoBuilder().comEstabelecimento(estabelecimento).construir();
+                produtoRepository.save(produto);
 
-        Iterable<Produto> buscaProduto = produtoRepository.findAll();
-        long quantidadeEncontrada = buscaProduto.spliterator().getExactSizeIfKnown();
+                String url = "/produto/" + produto.getId();
+                mockMvc.perform(delete(url)).andReturn();
 
-        assertThat(quantidadeEncontrada).isEqualTo(quantidadeEsperada);
+                Iterable<Produto> produtosEncontrados = produtoRepository.findAll();
+                long quantidadeEncontrada = produtosEncontrados.spliterator().getExactSizeIfKnown();
 
-    }
+                assertEquals(quantidadeEsperada, quantidadeEncontrada);
+        }
 
-    @Test
-    public void deve_incluir_um_produto() throws Exception {
+        @Test
+        void deve_cadastrar_um_novo_produto() throws Exception {
 
-        Estabelecimento estabelecimento = new EstabelecimentoBuilder().construir();
-        estabelecimentoRepository.save(estabelecimento);
-        String nomeProduto = "Pinga";
-        Produto produto = new ProdutoBuilder().comNome(nomeProduto).comEstabelecimento(estabelecimento).construir();
-        produtoRepository.save(produto);
-        String json = toJSON(produto);
-        mvc.perform(post("/produto/").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isCreated());
+                Usuario usuario = new UsuarioBuilder().construir();
 
-        List<Produto> produtosRetornados = produtoRepository
-                .findByNomeContainingIgnoreCase(produto.getNome());
+                Estabelecimento estabelecimento = new EstabelecimentoBuilder().comUsuario(usuario).construir();
+                estabelecimentoRepository.save(estabelecimento);
 
-        Assertions.assertThat(produtosRetornados.size()).isEqualTo(1);
-        Assertions.assertThat(
-                produto.getNome())
-                .isIn(produtosRetornados.stream().map(Produto::getNome).toList());
-    }
+                ProdutoRequestDTO produtoDTO = new ProdutoRequestDTO("pinga", "testezinho", "skol", 99.90,
+                                Categoria.Fermentada, Imagem.getBytes(), estabelecimento.getId());
 
-    private String toJSON(Produto produto) throws JsonProcessingException {
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(produto);
-        return json;
-    }
-
-    @Test
-    void deve_criar_um_produto() throws Exception {
-
-        Estabelecimento estabelecimento = new EstabelecimentoBuilder().construir();
-        estabelecimentoRepository.save(estabelecimento);
-
-        Produto produto = new ProdutoBuilder().comEstabelecimento(estabelecimento).construir();
-        produtoRepository.save(produto);
-
-        ProdutoRequestDTO produtoRequestDTO = new ProdutoRequestDTO();
-
-        mvc
-                .perform(
-                        post("/produto/")
+                mockMvc.perform(post("/produto")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(JsonUtil.toJson(produtoRequestDTO)))
-                .andExpect(status().isCreated());
+                                .content(JsonUtil.toJson(produtoDTO)))
+                                .andExpect(status().isCreated())
+                                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(jsonPath("$.nome").value("pinga"))
+                                .andExpect(jsonPath("$.descricao").value("testezinho"))
+                                .andExpect(jsonPath("$.marca").value("skol"))
+                                .andExpect(jsonPath("$.preco").value(99.90))
+                                .andExpect(jsonPath("$.imagem").value(Imagem.getBytes()))
+                                .andExpect(jsonPath("$.idEstabelecimento").value(estabelecimento.getUsuario().getId()))
+                                .andDo(result -> {
+                                        String jsonResponse = result.getResponse().getContentAsString();
+                                        ProdutoResponseDTO responseDTO = objectMapper.readValue(jsonResponse,
+                                                        ProdutoResponseDTO.class);
+                                        assertThat(responseDTO.getNome()).isEqualTo("pinga");
+                                        assertThat(responseDTO.getDescricao()).isEqualTo("testezinho");
+                                        assertThat(responseDTO.getMarca()).isEqualTo("skol");
+                                        assertThat(responseDTO.getPreco()).isEqualTo(99.90);
+                                        assertThat(responseDTO.getImagem()).isEqualTo(Imagem.getBytes());
+                                        assertThat(responseDTO.getIdEstabelecimento())
+                                                        .isEqualTo(responseDTO.getIdEstabelecimento());
+                                });
+        }
 
-        Iterable<Produto> produtosEncontrados = produtoRepository.findAll();
+        @Test
+        public void deve_alterar_dados_do_produto() throws Exception {
 
-        assertThat(produtosEncontrados)
-                .extracting("nome")
-                .containsOnly(produto.getNome());
-        assertThat(produtosEncontrados)
-                .extracting("descricao")
-                .containsOnly(produto.getDescricao());
-        assertThat(produtosEncontrados)
-                .extracting("marca")
-                .containsOnly(produto.getMarca());
-        assertThat(produtosEncontrados)
-                .extracting("preco")
-                .containsOnly(produto.getPreco());
-        assertThat(produtosEncontrados)
-                .extracting("categoria")
-                .containsOnly(produto.getCategoria());
-        assertThat(produtosEncontrados)
-                .extracting("imagem")
-                .containsOnly(produto.getImagem());
-        assertThat(produtosEncontrados)
-                .extracting("estabelecimento")
-                .containsOnly(produto.getEstabelecimento());
-    }
+                Usuario usuario = new UsuarioBuilder().construir();
 
-    @Test
-    public void deve_alterar_dados_do_produto() throws Exception {
-        Produto nomeAlterado = new ProdutoBuilder().construir();
-        produtoRepository.save(nomeAlterado);
+                Estabelecimento estabelecimento = new EstabelecimentoBuilder().comUsuario(usuario).construir();
+                estabelecimentoRepository.save(estabelecimento);
 
-        nomeAlterado.setNome("Pinga");
+                Produto produto = new ProdutoBuilder().comEstabelecimento(estabelecimento).construir();
 
-        String json = toJSON(nomeAlterado);
+                produtoRepository.save(produto);
 
-        this.mvc.perform(put("/produto").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isCreated());
+                ProdutoRequestDTO produtoDTO = new ProdutoRequestDTO("nomeAlterado", "descAlterada", "marcaAlterada",
+                                25.0, Categoria.Bitter, Imagem.getBytes(), estabelecimento.getUsuario().getId());
 
-        Produto produtoRetornado = produtoRepository.findById(nomeAlterado.getId()).get();
-        Assertions.assertThat(produtoRetornado.getId()).isEqualTo(nomeAlterado.getId());
-        Assertions.assertThat(produtoRetornado.getNome()).isEqualTo(nomeAlterado.getNome());
-    }
+                mockMvc.perform(put("/produto/{id}",
+                                produto.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(produtoDTO)))
+                                .andExpect(status().isOk());
+
+                Produto produtoAlterado = produtoRepository.findById(produto.getId())
+                                .orElse(null);
+                assertNotNull(produtoAlterado);
+                assertEquals("nomeAlterado", produtoAlterado.getNome());
+                assertEquals("descAlterada", produtoAlterado.getDescricao());
+                assertEquals("marcaAlterada", produtoAlterado.getMarca());
+                assertEquals(25.0, produtoAlterado.getPreco());
+                assertEquals(Categoria.Destilada, produtoAlterado.getCategoria());
+                assertEquals(Imagem.getBytes(), produtoAlterado.getImagem());
+                assertEquals(produto.getId(), produtoAlterado.getEstabelecimento().getId());
+        }
+
+        private String asJsonString(Object object) throws JsonProcessingException {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.writeValueAsString(object);
+        }
+
 }
