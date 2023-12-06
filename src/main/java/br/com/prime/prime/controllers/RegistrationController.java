e87088c923d916b1316b4f5a19d97e4f5501f53a
clevertonx <clevertonx@gmail.com>
2023-12-05 23:43:37 -0400

    ResetSenhaOk

--- src/main/java/br/com/prime/prime/controllers/RegistrationController.java
+++ src/main/java/br/com/prime/prime/controllers/RegistrationController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import br.com.prime.prime.Services.UserService;
 
